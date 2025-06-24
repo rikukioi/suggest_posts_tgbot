fd683068fb89a30c40d474a4580fc33411413ab3
rikukioi <alexandr.petrenko.24@mail.ru>
2025-06-24 16:59:59 +0500

    Исправлены хендлеры

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -7,12 +7,12 @@ from os import getenv
 from aiogram import Bot, F, Router
 from aiogram.types import CallbackQuery
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.orm import Session
 
-from src.database.engine import get_db
-from src.filters import IsAdmin
 from src.database.models import Post
+from src.filters import IsAdmin
+from src.middlewares import DatabaseMiddleware
 
 
 logger = logging.getLogger("tg_handlers")
@@ -22,18 +22,19 @@ channel_id = int(getenv("CHANNEL_ID"))
 admin_id = int(getenv("ADMIN_ID"))
 
 router = Router(name="admin_commands_router")
-router.message.filter(IsAdmin(admin_id))
 router.callback_query.filter(IsAdmin(admin_id))
+router.callback_query.middleware(DatabaseMiddleware())
 
 
 @router.callback_query(F.data == "post_approve")
-async def approve_post(callback: CallbackQuery, bot: Bot) -> None:
+async def approve_post(callback: CallbackQuery, bot: Bot, session: AsyncSession) -> None:
     logger.info("Админ id=%s одобрил публикацию", admin_id)
     await callback.message.edit_reply_markup(reply_markup=None)
-    db: Session = next(get_db())
-    post_id = hash(callback.data)
-    await db.execute(select(Post).filter_by(id=post_id)).scalar_one_or_none()
-    db.commit()
+
+    # FIXME: Зач это здесь? чето не увидел продолжения логики
+    # post_id = hash(callback.data)
+    # await session.execute(select(Post).filter_by(id=post_id))
+
     message = callback.message
     await bot.send_photo(chat_id=channel_id, photo=message.photo[-1].file_id, caption=message.caption)
     await callback.answer("Пост опубликован")
@@ -41,12 +42,11 @@ async def approve_post(callback: CallbackQuery, bot: Bot) -> None:
 
 
 @router.callback_query(F.data == "post_decline")
-async def decline_post(callback: CallbackQuery) -> None:
+async def decline_post(callback: CallbackQuery, session: AsyncSession) -> None:
     logger.info("Админ id=%s отклонил публикацию", admin_id)
     await callback.message.edit_reply_markup(reply_markup=None)
-    db: Session = next(get_db())
+
     post_id = hash(callback.data)
-    post = db.execute(select(Post).filter_by(id=post_id)).scalar_one_or_none()
-    db.delete(post)
-    db.commit()
+    session.delete(select(Post).filter_by(id=post_id))
+
     await callback.answer("Пост отклонен")
--- src/handlers/user.py
+++ src/handlers/user.py
@@ -5,17 +5,16 @@ from os import getenv
 from aiogram import Bot, F, Router
 from aiogram.types import Message
 from dotenv import load_dotenv
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.future import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.engine import get_db
+from src.database.models import Post
 from src.keyboards.inline.inline_admin import post_approval
-from src.database.models import Post, User
+from src.middlewares import DatabaseMiddleware
 
 
 load_dotenv()
 router = Router(name="user_commands_router")
+router.message.middleware(DatabaseMiddleware())
 logger = logging.getLogger("tg_handlers")
 admin_chat = getenv("ADMIN_CHAT_ID")
 
@@ -30,40 +29,22 @@ async def start_command(message: Message) -> None:
 
 
 @router.message(F.photo)
-async def post_suggest(message: Message, bot: Bot) -> None:
+async def post_suggest(message: Message, bot: Bot, session: AsyncSession) -> None:
     user = message.from_user
-    db: Session = next(get_db())
 
-    try:
-        db_user = db.execute(select(User).filter_by(telegram_id=user.id)).scalar_one_or_none()
-        if not db_user:
-            db_user = User(
-                telegram_id=user.id, username=user.username, first_name=user.first_name, last_name=user.last_name
-            )
-            db.add(db_user)
-            db.commit()
-            db.refresh(db_user)
+    new_post = Post(content_type="photo", file_id=message.photo[-1].file_id, caption=message.caption, user_id=user.id)
+    session.add(new_post)
+    await session.flush()
 
-        new_post = Post(
-            content_type="photo", file_id=message.photo[-1].file_id, caption=message.caption, user_id=db_user.id
+    await bot.send_message(
+        chat_id=admin_chat, text=f"Предложен пост от {user.first_name} {user.last_name} | {user.username}"
+    )
+    if message.content_type == "photo":
+        await bot.send_photo(
+            chat_id=admin_chat, photo=new_post.file_id, caption=new_post.caption, reply_markup=post_approval
         )
-        db.add(new_post)
-        db.commit()
-        db.refresh(new_post)
-
-        await bot.send_message(
-            chat_id=admin_chat, text=f"Предложен пост от {user.first_name} {user.last_name} | {user.username}"
-        )
-        if message.content_type == "photo":
-            await bot.send_photo(
-                chat_id=admin_chat, photo=new_post.file_id, caption=new_post.caption, reply_markup=post_approval
-            )
-            await message.reply(text="Вы нас балуете, милорд! Пост отправлен нашему горячо любимому барону...")
-        # elif message.content_type == "video":
-        #     await bot.send_video(
-        #         chat_id=admin_chat, video=message.video.file_id, caption=message.caption, reply_markup=post_approval
-        #     )
-
-    except SQLAlchemyError:
-        db.rollback()
-        await message.reply(text="Ошибка при обработке поста. Попробуйте позже.")
+        await message.reply(text="Вы нас балуете, милорд! Пост отправлен нашему горячо любимому барону...")
+    # elif message.content_type == "video":
+    #     await bot.send_video(
+    #         chat_id=admin_chat, video=message.video.file_id, caption=message.caption, reply_markup=post_approval
+    #     )
